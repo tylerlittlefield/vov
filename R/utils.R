6e4717bb5ad47e1f744986ee5574d2071fe06c27
tylurp <tylurp1@gmail.com>
2019-12-01 12:12:46 -0800

    pkgdown update

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,3 @@
-#' Transform NULL values in glue object
 null_transformer <- function(str = "NULL") {
   function(text, envir) {
     out <- glue::identity_transformer(text, envir)
@@ -10,7 +9,6 @@ null_transformer <- function(str = "NULL") {
   }
 }
 
-#' Parse animation class
 parse_animation <- function(class, duration, delay) {
   if (!is.null(delay))
     delay <- glue::glue("t-{delay}")

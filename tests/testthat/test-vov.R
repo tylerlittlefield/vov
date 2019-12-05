b0317eed277f84efe6a3071ee0a4af3f3fe9de4f
tylurp <tylurp1@gmail.com>
2019-12-05 07:31:41 -0800

    couple more tests

--- tests/testthat/test-vov.R
+++ tests/testthat/test-vov.R
@@ -15,9 +15,22 @@ test_that("vov works", {
     expected = '<h1 class="vov blur-out">hi</h1>'
   )
 
+  # fade in
   expect_equal(
     object = as.character(fade_in(h1("hi"))),
     expected = '<h1 class="vov fade-in">hi</h1>'
+  )
+
+  # fade_in_bottom_left
+  expect_equal(
+    object = as.character(fade_in_bottom_left(h1("hi"))),
+    expected = '<h1 class="vov fade-in-bottom-left">hi</h1>'
+  )
+
+  # fade_in_bottom_right
+  expect_equal(
+    object = as.character(fade_in_bottom_right(h1("hi"))),
+    expected = '<h1 class="vov fade-in-bottom-right">hi</h1>'
   )
 
 })
